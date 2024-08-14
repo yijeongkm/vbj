775572e04f093243d1f274dde297856452475a6a
yijeongkm <yijeongkm1@gmail.com>
2024-08-14 16:34:13 +0900

    Update images.js

--- api/images.js
+++ api/images.js
@@ -5,16 +5,12 @@ export default function handler(req, res) {
     const imagesDir = path.join(process.cwd(), 'public/assets/images');
     fs.readdir(imagesDir, (err, files) => {
         if (err) {
-            return res.status(500).json({ error: 'Unable to scan directory' });
+            return res.status(500).send('Unable to scan directory: ' + err);
         }
+        // 이미지 파일만 필터링
         const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/.test(file));
-        
-        if (imageFiles.length < 2) {
-            return res.status(500).json({ error: 'Not enough images to display' });
-        }
-        
-        // 이미지를 랜덤으로 섞어 두 개를 선택합니다.
-        const shuffledImages = imageFiles.sort(() => 0.5 - Math.random()).slice(0, 2);
-        res.status(200).json(shuffledImages);
+        // 이미지 경로를 URL 형식으로 반환
+        const imagePaths = imageFiles.map(file => `/assets/images/${file}`);
+        res.status(200).json(imagePaths);
     });
 }
