cade0fb8bbf2d9c09b2e4715c6e976d94d6418c6
yijeongkm <yijeongkm1@gmail.com>
2024-08-15 12:37:22 +0900

    Update images.js

--- api/images.js
+++ api/images.js
@@ -7,11 +7,8 @@ export default function handler(req, res) {
         if (err) {
             return res.status(500).send('Unable to scan directory: ' + err);
         }
-        // 이미지 파일만 필터링
         const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/.test(file));
-        // 랜덤으로 하나의 이미지를 선택
         const randomImage = imageFiles[Math.floor(Math.random() * imageFiles.length)];
-        // 선택된 이미지의 경로 반환
         const imagePath = `/assets/images/${randomImage}`;
         res.status(200).json({ imageUrl: imagePath });
     });
